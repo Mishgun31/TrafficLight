ac4ca82a23f36b883db28d06c37d31cf337dc099
Mikhail Mezentsev <mezya31@yandex.ru>
2021-03-25 02:25:27 +0300

    Constraint were added. Made the app's functionality.

---
+++ TrafficLight/TrafficLight.swift
@@ -0,0 +1,15 @@
+//
+//  TrafficLight.swift
+//  TrafficLight
+//
+//  Created by Михаил Мезенцев on 25.03.2021.
+//
+
+import Foundation
+
+enum TrafficLight {
+    case noLight
+    case req
+    case yellow
+    case green
+}
--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLight = TrafficLight.noLight
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        redLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 50
+        yellowLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 50
+        greenLightView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
     }
 
-
+    
+    @IBAction func startButtonPressed() {
+        switch currentLight {
+        case .noLight:
+            currentLight = .req
+            redLightView.alpha = 1
+        case .req:
+            currentLight = .yellow
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        case .yellow:
+            currentLight = .green
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        default:
+            currentLight = .req
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        }
+    }
 }
-
